07d2dd8f129b0457387f59fda4c8a349323a61d3
munique-progint <muniquerochadprado@gmail.com>
2026-07-19 12:57:29 -0300

    Implementa melhorias na página de conteúdos digitais

--- startbootstrap-modern-business-gh-pages/js/scripts.js
+++ startbootstrap-modern-business-gh-pages/js/scripts.js
@@ -5,3 +5,49 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+
+console.log("scripts.js carregado!");
+const favoritos = document.querySelectorAll(".favorito-card");
+
+console.log(favoritos);
+
+favoritos.forEach(function(favorito){
+
+    favorito.addEventListener("click", function(){
+
+        const coluna = favorito.closest(".col-lg-3");
+        const icone = favorito.querySelector("i");
+
+        if(window.location.pathname.includes("favoritos.html")){
+
+            icone.classList.remove("bi-heart-fill");
+            icone.classList.add("bi-heart");
+
+            coluna.classList.add("card-removendo");
+
+            setTimeout(function(){
+
+                coluna.remove();
+
+            },500);
+
+        }else{
+
+            if(icone.classList.contains("bi-heart")){
+
+                icone.classList.remove("bi-heart");
+                icone.classList.add("bi-heart-fill");
+
+            }else{
+
+                icone.classList.remove("bi-heart-fill");
+                icone.classList.add("bi-heart");
+
+            }
+
+        }
+
+    });
+
+});
